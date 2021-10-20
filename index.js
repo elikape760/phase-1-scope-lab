cba077dea27ee8d9fdd888750a461f4ea85bb095
Elias <elikapeluschnik@gmail.com>
2021-10-19 22:44:15 -0400

    commit tree 4babf69a43cda10fb7ccaffbae4046bce04ea219

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = "Eli"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob"
+}
